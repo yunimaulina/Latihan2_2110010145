1c64d62bcf3eeaea38ee8b99c21b2e353dd06e26
yunimaulina <yunimaulina30@gmail.com>
2023-06-13 08:54:10 +0800

    Melengkapi class mahasiswa

---
+++ src/universitas/MahasiswaBeraksi.java
@@ -0,0 +1,13 @@
+
+package universitas;
+
+public class MahasiswaBeraksi {
+     public static void main(String[] args) {
+    //membuat objek
+    Mahasiswa yuni = new Mahasiswa;
+    
+    System.out.println("Hasil penjuml : "+yuni.setPenjumlahan());
+    System.out.println("Hasil pengurangan : "+yuni.setPengurangan());
+    System.out.println("Hasil perkalian   : "+yuni.setPerkalian());
+    } 
+}
